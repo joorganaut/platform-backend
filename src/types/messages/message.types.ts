60e9183b98b215c837b04b8c6270d10f1eb94106
joorganaut <sazeespectra@gmail.com>
2021-07-13 20:14:55 -0400

    new features

--- src/types/messages/message.types.ts
+++ src/types/messages/message.types.ts
@@ -1,19 +1,29 @@
 import { Entity, BusinessObject } from "../admin/default.types"
 
+export enum MessageStatus {
+    sent,
+    pending,
+    failed
+}
+
 export interface Message extends BusinessObject {
     id?: string
+    title: string
     senderId: string
     receiverId: string
     status: string
+    error: string
     content: any[]
 
 }
 
 export interface MessageEntity extends Entity {
     id?: string
+    title: string
     sender_id: string
     receiver_id: string
     status: string
+    error: string
     content: string | any[]
 
 }
